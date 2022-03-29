9f33814f53a3fdf402c7017291a9ee25be6aa814
Brandon Reyes <brandon.reyesmi@gmail.com>
2022-03-29 12:06:28 -0600

    feat: implement project structure

---
+++ src/components/Search/SearchBarHOC.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import SearchBar from './SearchBar'
+
+const SearchBarHOC = () => {
+  return (
+    <SearchBar/>
+  )
+}
+
+export default SearchBarHOC
---
+++ src/views/FilterViewHOC.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import FilterView from './FilterView';
+import './FilterView.scss';
+
+const FIlterViewHOC = () => {
+  return (
+    <FilterView/>
+  )
+}
+
+export default FIlterViewHOC
